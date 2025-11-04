e403ff42c2f78987d2ac4771b72bf570eb97b534
Davy41 <gaibambasi95@gmail.com>
2025-11-04 16:36:32 +0200

    solved again

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({origin:"https://davyblog.netlify.app/"}));
+app.use(cors({origin:"https://davyblog.netlify.app"}));
 app.get("/health-check",(_,res)=>{
     res.json("Backend Is live  ...")
 })
