cf0f99ffae435521dcc59e860a643e3c22629e57
DAVY <hipfen1000@gmail.com>
2026-03-19 14:16:45 +0200

    change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({origin:["http://localhost:5173", "https://coosinblogs.netlify.app"]}));
+app.use(cors({origin:"http://localhost:5173"}));
 app.get("/health-check",(_,res)=>{
     res.json("Backend Is live  ...")
 })
