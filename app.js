8ec4878a9dd9a2dfbaab8ff909665a7903f33e12
DAVY <hipfen1000@gmail.com>
2026-03-19 15:36:10 +0200

    rollback to working commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({origin:"http://localhost:5173"}));
+app.use(cors({origin:"https://coosinblogs.netlify.app"}));
 app.get("/health-check",(_,res)=>{
     res.json("Backend Is live  ...")
 })
